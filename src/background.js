c1485a358b416d7b1698147363a4d2d897904bd0
watain666 <watain666666@gmail.com>
2019-08-30 15:18:23 +0800

    Modify use vue js refactor [#5] Add right click icon to open chhoe taigi home page [#6] Add option page

--- src/background.js
+++ src/background.js
@@ -1,79 +1,85 @@
-setContextMenus();
+global.browser = require('webextension-polyfill')
+
+setContextMenus()
 
 chrome.contextMenus.onClicked.addListener(info => {
-  const sMenuItemId = info.menuItemId;
-  const sSelection = info.selectionText;
-  const aResult = sMenuItemId.split("-");
-  const sUrl = getSearchUrl(sSelection, aResult[0], aResult[1]);
-  chrome.tabs.create({ url: sUrl });
-});
+  const menuItemId = info.menuItemId
+  const selection = info.selectionText
+  const result = menuItemId ? menuItemId.split('-') : []
+  const url = getSearchUrl(selection, result[0], result[1])
+  chrome.tabs.create({ url: url })
+})
 
-function setContextMenus() {
-  chrome.contextMenus.removeAll(function() {
+export function setContextMenus () {
+  chrome.contextMenus.removeAll(function () {
     chrome.contextMenus.create({
-      id: "poj-equals",
-      title: "Pe̍h-ōe-jī (Kāng款--ê)",
-      contexts: ["selection"]
-    });
+      id: 'poj-equals-open-chhoe-taigi',
+      type: 'normal',
+      title: '開 Chhoe Taigi 找台語 佇新頁',
+      contexts: ['browser_action']
+    })
     chrome.contextMenus.create({
-      id: "poj-contains",
-      title: "Pe̍h-ōe-jī (相關--ê)",
-      contexts: ["selection"]
-    });
+      id: 'poj-equals',
+      title: 'Pe̍h-ōe-jī (Kāng款--ê)',
+      contexts: ['selection']
+    })
     chrome.contextMenus.create({
-      id: "hoalo-equals",
-      title: "教育部羅馬字 (Kāng款--ê)",
-      contexts: ["selection"]
-    });
+      id: 'poj-contains',
+      title: 'Pe̍h-ōe-jī (相關--ê)',
+      contexts: ['selection']
+    })
     chrome.contextMenus.create({
-      id: "hoalo-contains",
-      title: "教育部羅馬字 (相關--ê)",
-      contexts: ["selection"]
-    });
+      id: 'hoalo-equals',
+      title: '教育部羅馬字 (Kāng款--ê)',
+      contexts: ['selection']
+    })
     chrome.contextMenus.create({
-      id: "hanlo-equals",
-      title: "漢字/漢羅合用 (Kāng款--ê)",
-      contexts: ["selection"]
-    });
+      id: 'hoalo-contains',
+      title: '教育部羅馬字 (相關--ê)',
+      contexts: ['selection']
+    })
     chrome.contextMenus.create({
-      id: "hanlo-contains",
-      title: "漢字/漢羅合用 (相關--ê)",
-      contexts: ["selection"]
-    });
+      id: 'hanlo-equals',
+      title: '漢字/漢羅合用 (Kāng款--ê)',
+      contexts: ['selection']
+    })
     chrome.contextMenus.create({
-      id: "hoabun-equals",
-      title: "對應華語 (Kāng款--ê)",
-      contexts: ["selection"]
-    });
+      id: 'hanlo-contains',
+      title: '漢字/漢羅合用 (相關--ê)',
+      contexts: ['selection']
+    })
     chrome.contextMenus.create({
-      id: "hoabun-contains",
-      title: "對應華語 (相關--ê)",
-      contexts: ["selection"]
-    });
+      id: 'hoabun-equals',
+      title: '對應華語 (Kāng款--ê)',
+      contexts: ['selection']
+    })
     chrome.contextMenus.create({
-      id: "english-equals",
-      title: "對應英文 (Kāng款--ê)",
-      contexts: ["selection"]
-    });
+      id: 'hoabun-contains',
+      title: '對應華語 (相關--ê)',
+      contexts: ['selection']
+    })
     chrome.contextMenus.create({
-      id: "english-contains",
-      title: "對應英文 (相關--ê)",
-      contexts: ["selection"]
-    });
-  });
+      id: 'english-equals',
+      title: '對應英文 (Kāng款--ê)',
+      contexts: ['selection']
+    })
+    chrome.contextMenus.create({
+      id: 'english-contains',
+      title: '對應英文 (相關--ê)',
+      contexts: ['selection']
+    })
+  })
 }
 
-function getSearchUrl(selectionText, spellingMethod, searchMethod) {
-  const aMenuItem = {
-    poj: "spellingMethod=poj_unicode&spelling=",
-    hoalo: "spellingMethod=kiplmj_unicode&spelling=",
-    hanlo: "taibun=",
-    hoabun: "hoabun=",
-    english: "english_descriptions="
-  };
-  let sUrl = "https://chhoe.taigi.info/";
-  if (selectionText) {
-    sUrl += `search?method=basic&searchMethod=${searchMethod}&${aMenuItem[spellingMethod]}${selectionText}`;
+export function getSearchUrl (selectionText, spellingMethod, searchMethod) {
+  const menuItems = {
+    poj: 'spellingMethod=poj_unicode&spelling=',
+    hoalo: 'spellingMethod=kiplmj_unicode&spelling=',
+    hanlo: 'taibun=',
+    hoabun: 'hoabun=',
+    english: 'english_descriptions='
   }
-  return sUrl;
+  let url = 'https://chhoe.taigi.info/'
+  url += selectionText ? `search?method=basic&searchMethod=${searchMethod}&${menuItems[spellingMethod]}${selectionText}` : ''
+  return url
 }
---
+++ src/options/App.vue
@@ -0,0 +1,217 @@
+<template>
+  <div>
+    <div id="setting">
+      <h3>Sek-téng tiám icon beh án-nóa chhōe:</h3>
+      <ul>
+        <li>
+          <span>Kéng ê 方式:</span>
+          <span v-for="(value, key) in searchMethods" :key="key">
+            <label>
+              <input type="radio" v-model="searchMethod" :value="key" class="form-radio">
+              {{ value }}
+            </label>
+          </span>
+        </li>
+        <li>
+          <span>Chhōe ê 方式:</span>
+          <select v-model="spellingMethod" class="select-box">
+            <option v-for="(value, key) in spellingMethods" :key="key" :value="key">
+              {{ value }}
+            </option>
+          </select>
+        </li>
+      </ul>
+      <button @click="configSave()">Submit</button>
+        <transition name="msg">
+          <span id="savedMessage" v-show="savedMessage">Saved</span>
+        </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+import Browser from 'webextension-polyfill'
+export default {
+  name: 'App',
+
+  data () {
+    return {
+      savedMessage: false,
+      searchMethod: '',
+      searchMethods: {
+        'equals': 'Kāng款--ê',
+        'contains': '相關--ê',
+      },
+      spellingMethod: '',
+      spellingMethods: {
+        'poj': 'Pe̍h-ōe-jī',
+        'hoalo': '教育部羅馬字',
+        'hanlo': '漢字/漢羅合用',
+        'hoabun': '對應華語',
+        'english': '對應英文',
+      }
+    }
+  },
+  mounted () {
+    this.defaultConfig()
+  },
+  methods: {
+    async defaultConfig () {
+      const data = await Browser.storage.local.get({
+        searchMethod: 'equals',
+        spellingMethod: 'poj'
+      })
+      this.searchMethod = data.searchMethod
+      this.spellingMethod = data.spellingMethod
+    },
+    configSave () {
+      Browser.storage.local.set(
+        {
+          searchMethod: this.searchMethod,
+          spellingMethod: this.spellingMethod,
+        }
+      )
+      this.savedMessage = true
+      setTimeout(() => {
+        this.savedMessage = false
+      }, 1000)
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  #setting {
+    padding: 5px 5px 5px 5px;
+    color: #333;
+    font: 15px/1.7 'Open Sans', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    ul {
+      list-style: none;
+      padding-left: 0;
+    }
+
+    li {
+      padding: 5px 5px 5px 5px;
+    }
+
+    #savedMessage {
+      margin-left: 5px;
+    }
+
+    .msg-enter-active,
+    .msg-leave-active {
+      transition: opacity 0.5s;
+    }
+    .msg-enter,
+    .msg-leave-to {
+      opacity: 0;
+    }
+
+    button {
+      z-index: 1;
+      position: relative;
+      font-size: inherit;
+      font-family: inherit;
+      color: white;
+      padding: 0.5em 1em;
+      outline: none;
+      border: none;
+      background-color: #5b9564;
+      overflow: hidden;
+      cursor: pointer;
+    }
+
+    button::after {
+      content: '';
+      z-index: -1;
+      background-color: hsla(0, 0%, 100%, 0.2);
+      position: absolute;
+      top: -50%;
+      bottom: -50%;
+      width: 1.25em;
+      transform: translate3d(-525%, 0, 0) rotate(35deg);
+    }
+
+    button:hover::after {
+      transition: transform 0.45s ease-in-out;
+      transform: translate3d(200%, 0, 0) rotate(35deg);
+    }
+
+    .form-radio
+    {
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+      display: inline-block;
+      position: relative;
+      background-color: #f1f1f1;
+      color: #666;
+      top: 4px;
+      height: 15px;
+      width: 15px;
+      border: 0;
+      border-radius: 50px;
+      cursor: pointer;
+      margin: 0px 0px 0px 9px;
+      outline: none;
+    }
+    .form-radio:checked::before
+    {
+      position: absolute;
+      font: 13px/1 'Open Sans', sans-serif;
+      left: 4px;
+      top: 0px;
+      content: '\02143';
+      transform: rotate(40deg);
+    }
+    .form-radio:hover
+    {
+      background-color: #f7f7f7;
+    }
+    .form-radio:checked
+    {
+      background-color: #f1f1f1;
+    }
+    label
+    {
+      cursor: pointer;
+    }
+
+    .select-box
+    {
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+      width: 200px;
+      padding: 9px 9px 11px 13px;
+      background-color: #f1f1f1;
+      border: none;
+      border-radius: 3px;
+      outline: none;
+      font: 15px 'Open Sans', sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      color: #666;
+      cursor: pointer;
+    }
+    .select-point
+    {
+      position: absolute;
+      top: 8px;
+      right: 13px;
+      font: 16px 'Open Sans', sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      color: #666;
+    }
+    select:-moz-focusring
+    {
+      color: transparent;
+      text-shadow: 0 0 0 #000;
+    }
+  }
+
+</style>
